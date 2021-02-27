c67ab191d85a6a78d2e79c08dbf5379fe8aca070
Maxim Kalevich <maxkalevich@gmail.com>
2021-02-27 18:00:24 +0300

    init: completed the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+
+module.exports = function reverse(n) {
+    let num = n.toString().split("").reverse().join("");
+    parseInt(num)
+   return parseInt(num)
 }
